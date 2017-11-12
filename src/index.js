4cbce18486e0438be796ffcfda4e015d52a624b5
Samuel Kortchmar <Samuel.Kortchmar@spacex.com>
2017-11-11 20:57:38 -0800

    linted except for set

--- src/index.js
+++ src/index.js
@@ -1,124 +1,119 @@
-import { createSelector } from 'reselect';
-const _registered = {};
-let _getState = null;
-let _allSelectors = new Set();
+import { createSelector } from 'reselect'
+const _registered = {}
+let _getState = null
+let _allSelectors = new Set()
 
 
-const _isFunction = (func) => typeof func === 'function';
+const _isFunction = (func) => typeof func === 'function'
 
 export function createSelectorWithDependencies(...funcs) {
-  let resultFunc = funcs.pop();
-  const dependencies = Array.isArray(funcs[0]) ? funcs[0] : funcs;
-  const selector = createSelector(...dependencies, resultFunc);
-  selector.dependencies = dependencies;
-  _allSelectors.add(selector);
-  return selector;
+  let resultFunc = funcs.pop()
+  const dependencies = Array.isArray(funcs[0]) ? funcs[0] : funcs
+  const selector = createSelector(...dependencies, resultFunc)
+  selector.dependencies = dependencies
+  _allSelectors.add(selector)
+  return selector
 }
 
 
 export function registerSelectors(selectors) {
-  const actuallySelectors = {};
+  const actuallySelectors = {}
   Object.keys(selectors).forEach((key) => {
-    const selector = selectors[key];
+    const selector = selectors[key]
     if (selector.resultFunc || _isFunction(selector)) {
-      actuallySelectors[key] = selector;
+      actuallySelectors[key] = selector
     }
-  });
-  return Object.assign(_registered, actuallySelectors);
+  })
+  return Object.assign(_registered, actuallySelectors)
 }
 
 function _unregisterSelectors() {
   Object.keys(_registered).forEach((key) => {
-    delete _registered[key];
-  });
+    delete _registered[key]
+  })
 }
 
 
 export function reset() {
-  _unregisterSelectors();
-  _getState = null;
-  _allSelectors = new Set();
+  _unregisterSelectors()
+  _getState = null
+  _allSelectors = new Set()
 }
 
 
 export function checkSelector(selector) {
   if (typeof selector === 'string') {
-    selector = _registered[selector];
+    selector = _registered[selector]
   }
 
   if (!_isFunction(selector)) {
-    throw error(`Selector ${selector} is not a function`);
+    throw new Error(`Selector ${selector} is not a function`)
   }
 
-  const dependencies = selector.dependencies || [];
-  const recomputations = selector.recomputations ? selector.recomputations() : 'N/A/';
+  const dependencies = selector.dependencies || []
+  const recomputations = selector.recomputations ? selector.recomputations() : 'N/A/'
 
-  const ret = { dependencies, recomputations };
+  const ret = { dependencies, recomputations }
   if (_getState) {
-    const state = _getState();
-    const inputs = dependencies.map((parentSelector) => parentSelector(state));
-    const output = selector(state);
-    Object.assign(ret, { inputs, output });
+    const state = _getState()
+    const inputs = dependencies.map((parentSelector) => parentSelector(state))
+    const output = selector(state)
+    Object.assign(ret, { inputs, output })
   }
 
-  return ret;
+  return ret
 }
 
 
 export function getStateWith(stateGetter) {
-  _getState = stateGetter;
+  _getState = stateGetter
 }
 
-
-function splitOnce(str, token) {
-  const components = str.split(token);
-  return components.length === 1 ? components : [components.shift(), components.join(token)];
-}
 
 function _sumString(str) {
-  return Array.from(str.toString()).reduce((sum, char) => char.charCodeAt(0) + sum, 0);
+  return Array.from(str.toString()).reduce((sum, char) => char.charCodeAt(0) + sum, 0)
 }
 
 const defaultSelectorKey = (selector, registry) => {
   if (selector.name) { // if its a vanilla function, it will have a name.
-    return selector.name;
+    return selector.name
   }
 
   for (let key of Object.keys(registry)) {
     if (registry[key] === selector) {
-      return key;
+      return key
     }
   }
 
   return (selector.dependencies || []).reduce((base, dep) => {
-    return base + _sumString(dep);
-  }, selector.resultFunc.toString());
+    return base + _sumString(dep)
+  }, selector.resultFunc.toString())
 }
 
 export function selectorGraph(selectorKey = defaultSelectorKey) {
-    const graph = { nodes: {}, edges: [] };
-    const traversedDependencies = new Set();
+  const graph = { nodes: {}, edges: [] }
+  const traversedDependencies = new Set()
 
-    const addToGraph = (selector) => {
-      const name = selectorKey(selector, _registered);
-      graph.nodes[name] = {
-        recomputations: selector.recomputations ? selector.recomputations() : 'N/A',
-        name,
-      };
-
-      let dependencies = selector.dependencies || [];
-      if (traversedDependencies.has(name)) { // Don't re-add.
-        dependencies = [];
-      }
-      dependencies.forEach((dependency) => {
-        addToGraph(dependency);
-        graph.edges.push({ from: name, to: selectorKey(dependency, _registered) });
-      });
-      traversedDependencies.add(name);
+  const addToGraph = (selector) => {
+    const name = selectorKey(selector, _registered)
+    graph.nodes[name] = {
+      recomputations: selector.recomputations ? selector.recomputations() : 'N/A',
+      name
     }
 
-    for (let selector of _allSelectors) {
-      addToGraph(selector);
+    let dependencies = selector.dependencies || []
+    if (traversedDependencies.has(name)) { // Don't re-add.
+      dependencies = []
     }
-    return graph;
+    dependencies.forEach((dependency) => {
+      addToGraph(dependency)
+      graph.edges.push({ from: name, to: selectorKey(dependency, _registered) })
+    })
+    traversedDependencies.add(name)
+  }
+
+  for (let selector of _allSelectors) {
+    addToGraph(selector)
+  }
+  return graph
 }
--- test/test_selector.js
+++ test/test_selector.js
@@ -1,165 +1,165 @@
-import chai from 'chai';
+import chai from 'chai'
 import {  
   createSelectorWithDependencies,
   registerSelectors,
   getStateWith,
   checkSelector,
   selectorGraph,
-  reset  } from '../src/index';
+  reset  } from '../src/index'
 
 const assert = chai.assert
 
-beforeEach(reset);
+beforeEach(reset)
 
 suite('createSelectorWithDependencies', () => {
 
   test('it creates a selector which has dependencies', () => {
-    const func1 = () => 1;
-    const selector = createSelectorWithDependencies(func1, () => 1);
-    assert.equal(selector.dependencies.length, 1);
-    assert.equal(selector.dependencies[0], func1);
-  });
+    const func1 = () => 1
+    const selector = createSelectorWithDependencies(func1, () => 1)
+    assert.equal(selector.dependencies.length, 1)
+    assert.equal(selector.dependencies[0], func1)
+  })
 
   test('they can be spread', () => {
-    const func1 = (state) => state.func1;
-    const func2 = (state) => state.func2;
-    const func3 = (state) => state.func3;
+    const func1 = (state) => state.func1
+    const func2 = (state) => state.func2
+    const func3 = (state) => state.func3
     const selector = createSelectorWithDependencies(
-      func1, func2, func3, () => 1);
-    assert.equal(selector.dependencies.length, 3);
-    assert.equal(selector.dependencies[0], func1);
-    assert.equal(selector.dependencies[1], func2);
-    assert.equal(selector.dependencies[2], func3);
-  });
+      func1, func2, func3, () => 1)
+    assert.equal(selector.dependencies.length, 3)
+    assert.equal(selector.dependencies[0], func1)
+    assert.equal(selector.dependencies[1], func2)
+    assert.equal(selector.dependencies[2], func3)
+  })
 
   test('they can be an array', () => {
-    const func1 = (state) => state.func1;
-    const func2 = (state) => state.func2;
-    const func3 = (state) => state.func3;
+    const func1 = (state) => state.func1
+    const func2 = (state) => state.func2
+    const func3 = (state) => state.func3
     const selector = createSelectorWithDependencies(
-      [func1, func2, func3], () => 1);
-    assert.equal(selector.dependencies.length, 3);
-    assert.equal(selector.dependencies[0], func1);
-    assert.equal(selector.dependencies[1], func2);
-    assert.equal(selector.dependencies[2], func3);
-  });
+      [ func1, func2, func3 ], () => 1)
+    assert.equal(selector.dependencies.length, 3)
+    assert.equal(selector.dependencies[0], func1)
+    assert.equal(selector.dependencies[1], func2)
+    assert.equal(selector.dependencies[2], func3)
+  })
 
   test('they can be recursive', () => {
-    const func1 = (state) => state.func1;
-    const selector1 = createSelectorWithDependencies(func1, () => 'selector1');
+    const func1 = (state) => state.func1
+    const selector1 = createSelectorWithDependencies(func1, () => 'selector1')
 
-    const func2 = (state) => state.func2;
-    const func3 = (state) => state.func3;
+    const func2 = (state) => state.func2
+    const func3 = (state) => state.func3
     const selector = createSelectorWithDependencies(
-      [selector1, func2, func3], () => 1);
-    assert.equal(selector.dependencies.length, 3);
-    assert.equal(selector.dependencies[0], selector1);
-    assert.equal(selector.dependencies[1], func2);
-    assert.equal(selector.dependencies[2], func3);
+      [ selector1, func2, func3 ], () => 1)
+    assert.equal(selector.dependencies.length, 3)
+    assert.equal(selector.dependencies[0], selector1)
+    assert.equal(selector.dependencies[1], func2)
+    assert.equal(selector.dependencies[2], func3)
 
-    assert.equal(selector.dependencies[0].dependencies[0], func1);
-  });
-});
+    assert.equal(selector.dependencies[0].dependencies[0], func1)
+  })
+})
 
 suite('registerSelectors', () => {
 
   test('allows you to name selectors', () => {
-    const foo = () => 'foo';
-    const bar = createSelectorWithDependencies(foo, () => 'bar');
-    const baz = createSelectorWithDependencies(bar, foo, () => 'bar');
-    const registered = registerSelectors({ foo, bar, baz });
+    const foo = () => 'foo'
+    const bar = createSelectorWithDependencies(foo, () => 'bar')
+    const baz = createSelectorWithDependencies(bar, foo, () => 'bar')
+    const registered = registerSelectors({ foo, bar, baz })
 
-    assert.equal(Object.keys(registered).length, 3);
-    assert.equal(registered.foo, foo);
-    assert.equal(registered.bar, bar);
-    assert.equal(registered.baz, baz);
-  });
+    assert.equal(Object.keys(registered).length, 3)
+    assert.equal(registered.foo, foo)
+    assert.equal(registered.bar, bar)
+    assert.equal(registered.baz, baz)
+  })
 
   test('ignores inputs which are not selectors or functions', () => {
-    const foo = () => 'foo';
-    const bar = createSelectorWithDependencies(foo, () => 'bar');
+    const foo = () => 'foo'
+    const bar = createSelectorWithDependencies(foo, () => 'bar')
     const utilities = {
       identity: x => x
-    };
-    const selectors = { foo, bar, utilities };
-    const registered = registerSelectors(selectors);
+    }
+    const selectors = { foo, bar, utilities }
+    const registered = registerSelectors(selectors)
 
-    assert.equal(Object.keys(registered).length, 2);
-  });
+    assert.equal(Object.keys(registered).length, 2)
+  })
 
   test('can be called additively', () => {
-    const foo = () => 'foo';
-    const bar = createSelectorWithDependencies(foo, () => 'bar');
-    const baz = createSelectorWithDependencies(bar, foo, () => 'bar');
+    const foo = () => 'foo'
+    const bar = createSelectorWithDependencies(foo, () => 'bar')
+    const baz = createSelectorWithDependencies(bar, foo, () => 'bar')
 
 
-    let registered = registerSelectors({ foo, bar });
-    assert.equal(Object.keys(registered).length, 2);
+    let registered = registerSelectors({ foo, bar })
+    assert.equal(Object.keys(registered).length, 2)
     
-    registered = registerSelectors({ baz });
-    assert.equal(Object.keys(registered).length, 3);
+    registered = registerSelectors({ baz })
+    assert.equal(Object.keys(registered).length, 3)
 
-    registered = registerSelectors({ baz });
-    assert.equal(Object.keys(registered).length, 3);
-  });
-});
+    registered = registerSelectors({ baz })
+    assert.equal(Object.keys(registered).length, 3)
+  })
+})
 
 suite('checkSelector', () => {
 
   test('it outputs a selector\'s dependencies, even if it\'s a plain function', () => {
-    const foo = () => 'foo';
-    const bar = createSelectorWithDependencies(foo, () => 'bar');
+    const foo = () => 'foo'
+    const bar = createSelectorWithDependencies(foo, () => 'bar')
     
-    assert.equal(checkSelector(foo).dependencies.length, 0);
+    assert.equal(checkSelector(foo).dependencies.length, 0)
 
-    assert.equal(checkSelector(bar).dependencies.length, 1);
-    assert.equal(checkSelector(bar).dependencies[0], foo);
-  });
+    assert.equal(checkSelector(bar).dependencies.length, 1)
+    assert.equal(checkSelector(bar).dependencies[0], foo)
+  })
 
   test('if you give it a way of getting state, it also gets inputs and outputs', () => {
     const state = { 
       foo: {
         baz: 1
       }
-    };
+    }
 
-    getStateWith(() => state);
+    getStateWith(() => state)
 
-    const foo = (state) => state.foo;
-    const bar = createSelectorWithDependencies(foo, (foo) => foo.baz);
+    const foo = (state) => state.foo
+    const bar = createSelectorWithDependencies(foo, (foo) => foo.baz)
     
-    const checkedFoo = checkSelector(foo);
-    assert.equal(checkedFoo.inputs.length, 0);
-    assert.deepEqual(checkedFoo.output, { baz: 1 });
-    assert.deepEqual(checkedFoo.output, foo(state));
+    const checkedFoo = checkSelector(foo)
+    assert.equal(checkedFoo.inputs.length, 0)
+    assert.deepEqual(checkedFoo.output, { baz: 1 })
+    assert.deepEqual(checkedFoo.output, foo(state))
 
-    const checkedBar = checkSelector(bar);    
-    assert.deepEqual(checkedBar.inputs, [{ baz: 1 }]);
-    assert.equal(checkedBar.output, 1);
-    assert.deepEqual(checkedBar.output, bar(state));
+    const checkedBar = checkSelector(bar)    
+    assert.deepEqual(checkedBar.inputs, [ { baz: 1 } ])
+    assert.equal(checkedBar.output, 1)
+    assert.deepEqual(checkedBar.output, bar(state))
 
-    getStateWith(null);
+    getStateWith(null)
   })
 
   test('it returns the number of recomputations for a given selector', () => {
-    const foo = (state) => state.foo;
-    const bar = createSelectorWithDependencies(foo, (foo) => foo.baz);
-    assert.equal(bar.recomputations(), 0);
+    const foo = (state) => state.foo
+    const bar = createSelectorWithDependencies(foo, (foo) => foo.baz)
+    assert.equal(bar.recomputations(), 0)
 
     const state = { 
       foo: {
-        baz: 1,
+        baz: 1
       }
-    };
-    getStateWith(() => state);
+    }
+    getStateWith(() => state)
 
-    bar(state);
-    assert.equal(bar.recomputations(), 1);
-    bar(state);
+    bar(state)
+    assert.equal(bar.recomputations(), 1)
+    bar(state)
 
     assert.deepEqual(checkSelector(bar), {
       dependencies: [ foo ],
-      inputs: [{ baz : 1}],
+      inputs: [ { baz : 1 } ],
       output: 1,
       recomputations: 1
     })
@@ -168,47 +168,47 @@ suite('checkSelector', () => {
       foo: {
         baz: 2
       }
-    };
-    getStateWith(() => newState);
+    }
+    getStateWith(() => newState)
 
-    bar(newState);
-    assert.equal(bar.recomputations(), 2);
+    bar(newState)
+    assert.equal(bar.recomputations(), 2)
 
-    bar(newState);
+    bar(newState)
     assert.deepEqual(checkSelector(bar), {
       dependencies: [ foo ],
-      inputs: [{ baz : 2}],
+      inputs: [ { baz : 2 } ],
       output: 2,
       recomputations: 2
-    });
-  });
+    })
+  })
 
   test("it allows you to pass in a string name of a selector if you've registered", () => {
-    const foo = (state) => state.foo;
-    const bar = createSelectorWithDependencies(foo, (foo) => foo + 1);
-    registerSelectors({ bar });
-    getStateWith(() => ({foo: 1}));
-    const checked = checkSelector('bar');
+    const foo = (state) => state.foo
+    const bar = createSelectorWithDependencies(foo, (foo) => foo + 1)
+    registerSelectors({ bar })
+    getStateWith(() => ({ foo: 1 }))
+    const checked = checkSelector('bar')
     assert.deepEqual(checked, {
       dependencies: [ foo ],
-      inputs: [1],
+      inputs: [ 1 ],
       output: 2,
-      recomputations: 0,
-    });
-  });
-});
+      recomputations: 0
+    })
+  })
+})
 
 suite('selectorGraph', () => {
   function createMockSelectors() {
-    const data$ = (state) => state.data;
-    const ui$ = (state) => state.ui;
-    const users$ = createSelectorWithDependencies(data$, (data) => data.users);
-    const pets$ = createSelectorWithDependencies(data$, ({ pets }) => pets);
-    const currentUser$ = createSelectorWithDependencies(ui$, users$, (ui, users) => users[ui.currentUser]);
-    const currentUserPets$ = createSelectorWithDependencies(currentUser$, pets$, (currentUser, pets) => currentUser.pets.map((petId) => pets[petId]));
-    const random$ = (state) => 1;
-    const thingy$ = createSelectorWithDependencies(random$, (number) => number + 1);
-    const booya$ = createSelectorWithDependencies(thingy$, currentUser$, (x, y) => 'booya!');
+    const data$ = (state) => state.data
+    const ui$ = (state) => state.ui
+    const users$ = createSelectorWithDependencies(data$, (data) => data.users)
+    const pets$ = createSelectorWithDependencies(data$, ({ pets }) => pets)
+    const currentUser$ = createSelectorWithDependencies(ui$, users$, (ui, users) => users[ui.currentUser])
+    const currentUserPets$ = createSelectorWithDependencies(currentUser$, pets$, (currentUser, pets) => currentUser.pets.map((petId) => pets[petId]))
+    const random$ = () => 1
+    const thingy$ = createSelectorWithDependencies(random$, (number) => number + 1)
+    const booya$ = createSelectorWithDependencies(thingy$, currentUser$, () => 'booya!')
     return {
       data$,
       ui$,
@@ -218,38 +218,38 @@ suite('selectorGraph', () => {
       currentUserPets$,
       random$,
       thingy$,
-      booya$,    
-    };
+      booya$    
+    }
   }
 
   test('it outputs a selector graph', () => {
-    const selectors = createMockSelectors();
-    const { edges, nodes } = selectorGraph();
-    assert.equal(Object.keys(nodes).length, Object.keys(selectors).length);
-    assert.equal(edges.length, 9);
-  });
+    const selectors = createMockSelectors()
+    const { edges, nodes } = selectorGraph()
+    assert.equal(Object.keys(nodes).length, Object.keys(selectors).length)
+    assert.equal(edges.length, 9)
+  })
 
   test('it names the nodes based on their string name by default', () => {
-    const selectors = createMockSelectors();
-    const { edges, nodes } = selectorGraph();
+    createMockSelectors()
+    const { nodes } = selectorGraph()
 
     // comes from func.name for top-level vanilla selector functions.
-    assert.equal(nodes['data$'].recomputations, 'N/A');
-  });
+    assert.equal(nodes['data$'].recomputations, 'N/A')
+  })
 
   test("doesn't duplicate nodes if they are different", () => {
-    const foo$ = (state) => state.foo; // node1
-    const select = (state) => 1;
-    const selector1 = createSelectorWithDependencies(foo$, select); // node 2
-    const selector2 = createSelectorWithDependencies(select); // node 3
-    const { nodes } = selectorGraph();
-    assert.equal(Object.keys(nodes).length, 3);
-  });
+    const foo$ = (state) => state.foo // node1
+    const select = () => 1
+    createSelectorWithDependencies(foo$, select) // node 2
+    createSelectorWithDependencies(select) // node 3
+    const { nodes } = selectorGraph()
+    assert.equal(Object.keys(nodes).length, 3)
+  })
 
   test('it names the nodes based on entries in the registry if they are there', () => {
-    const selectors = createMockSelectors();
-    registerSelectors(selectors);
-    const { edges, nodes } = selectorGraph();
+    const selectors = createMockSelectors()
+    registerSelectors(selectors)
+    const { edges } = selectorGraph()
 
     const expectedEdges = [ 
       { from: 'users$', to: 'data$' },
@@ -262,6 +262,6 @@ suite('selectorGraph', () => {
       { from: 'booya$', to: 'thingy$' },
       { from: 'booya$', to: 'currentUser$' }
     ]
-    assert.sameDeepMembers(edges, expectedEdges);
+    assert.sameDeepMembers(edges, expectedEdges)
   })
-});
+})
